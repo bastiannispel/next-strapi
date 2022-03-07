d3bd465df202ddbd162615617f880b0e2406d9e1
Bastian Nispel <b.nispel@t-online.de>
2022-03-07 20:10:32 +0100

    CI (#3)
    * setup husky and lint-staged
    
    * setup husky hook policy
    
    * added ci workflow
    
    * changed main-branch-name
    
    * added update workflow
    
    * modified update workflow
    
    * setup github environments for ci/cd
    
    * added strapi dependencies to root package json

--- apps/strapi/config/admin.js
+++ apps/strapi/config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '0c9dc8fe38df0df649b560d16e42483e'),
+    secret: env('JWT_SECRET', '0c9dc8fe38df0df649b560d16e42483e'),
   },
 });
